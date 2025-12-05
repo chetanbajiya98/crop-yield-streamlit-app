8f03230b50000179fa5957781fffaf1dd667336c
chetanbajiya98 <chetanbajiya98@gmail.com>
2025-12-05 14:09:44 +0530

    app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.set_page_config(page_title="Crop Yield Prediction", layout="centered")
 # 1️⃣ DOWNLOAD + LOAD MACHINE LEARNING MODEL
 # =====================================================================
 
-MODEL_URL = "https://huggingface.co/spaces/chetanbajiya/crop-yield-api/resolve/main/yield_model_streamlit.pkl"
-MODEL_PATH = "yield_model_streamlit.pkl"
+MODEL_URL = "https://huggingface.co/spaces/chetanbajiya/crop-yield-api/resolve/main/yield_model5.pkl"
+MODEL_PATH = "yield_model5.pkl"
 
 @st.cache_resource
 def load_model():
